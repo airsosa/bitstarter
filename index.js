04b0371b3cfe8e2ee052f4a81eee35540ec9c7ec
Eseosa Aigbogun <eseosa.aigbogun@conzultrix.com>
2017-02-18 02:37:02 +0100

    changed access port to 8080 and added bs css to header in index.html

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var buffer = fs.readFileSync('index.html');
 
 var output = buffer.toString('utf-8')
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
