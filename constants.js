9be5f08a77e25084ea753f35771d0deb163a3ff4
Eseosa Aigbogun <eseosa.aigbogun@conzultrix.com>
2019-09-16 21:40:24 +0100

    added meta tag and set end date

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 const CONSTANTS = {
   FUNDING_TARGET: 55000.00,
   FUNDING_CURRENCY: "USD",
-  CURRENCY_RATE: 370,
-  FUNDING_END_DATE: new Date("April 8, 2019"),
+  CURRENCY_RATE: 360,
+  FUNDING_END_DATE: new Date("January 15, 2020"),
   TWITTER_USERNAME: "conzultrix",
   TWITTER_TWEET: "I just supported Conzultrix Impact, you too can contribute to developing women and children in STEM.",
   days_left: function() {
